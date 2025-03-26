da17f8a5c8bb7df61e26e81039b2975c3b0d082c
Denis Dobanda <denis.dobanda@exxeta.com>
2025-03-26 11:05:38 +0100

    feat: improved hooks detection
    Signed-off-by: Denis Dobanda <denis.dobanda@exxeta.com>

--- lib/src/threat_detection_center.dart
+++ lib/src/threat_detection_center.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter_security_toolkit/src/threat_center_api.g.dart';
 
 /// Use this class to access the methods of detecting security threats
@@ -16,8 +18,10 @@ class ThreatDetectionCenter {
   /// libraries.
   ///
   /// Returns true, if any hooks are detected
-  static Future<bool?> areHooksDetected() {
-    return _api.areHooksDetected();
+  static Future<bool?> areHooksDetected() async {
+    return Socket.connect("127.0.0.1", 27042)
+        .then((_) => true)
+        .catchError((_) => _api.areHooksDetected());
   }
 
   /// Will check for an environment and return true, if running in simulated /
